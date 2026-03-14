f78864b42b20958bd7eb6e9dbdc48cdefb778c6c
Gate Guardian <gateguardian@qq.com>
2026-03-14 02:03:01 +0000

    update workflow

--- .github/scripts/meta.sh
+++ .github/scripts/meta.sh
@@ -2,12 +2,19 @@
 
 set -euo pipefail
 
+name="$(tomlq -r '.name' < pack.toml)"
+slug="$(printf '%s' "$name" | tr '[:upper:]' '[:lower:]' | sed -E 's/[^a-z0-9]+/-/g; s/^-+|-+$//g')"
 version="$(tomlq -r '.version' < pack.toml)"
-slug="$(tomlq -r '.name' < pack.toml | tr '[:upper:]' '[:lower:]' | sed -E 's/[^a-z0-9]+/-/g; s/^-+|-+$//g')"
 minecraft_version="$(tomlq -r '.versions.minecraft' < pack.toml)"
 forge_version="$(tomlq -r '.versions.forge' < pack.toml)"
+short_sha="${GITHUB_SHA::7}"
 
-echo "VERSION=$version" >> "$GITHUB_ENV"
-echo "SLUG=$slug" >> "$GITHUB_ENV"
-echo "MINECRAFT_VERSION=$minecraft_version" >> "$GITHUB_ENV"
-echo "FORGE_VERSION=$forge_version" >> "$GITHUB_ENV"
+{
+    echo "NAME=$name"
+    echo "SLUG=$slug"
+    echo "VERSION=$version"
+    echo "MINECRAFT_VERSION=$minecraft_version"
+    echo "FORGE_VERSION=$forge_version"
+    echo "SHORT_SHA"="$short_sha"
+} >> "$GITHUB_ENV"
+
