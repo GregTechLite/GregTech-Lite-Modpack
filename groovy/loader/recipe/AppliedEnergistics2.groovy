5e69c021076017922052155865af5b569f633706
Magic_Sweepy <Magic_Sweepy@proton.me>
2026-06-09 09:47:59 +0800

    rebalance dye color consume amount in appeng cable coloring

--- groovy/loader/recipe/AppliedEnergistics2.groovy
+++ groovy/loader/recipe/AppliedEnergistics2.groovy
@@ -2166,7 +2166,7 @@ for (int i = 0; i < 16; i++) {
     // Glass Cable + Chem Dye -> Colored Glass Cable
     chemical_bath.recipeBuilder()
         .inputs(item('appliedenergistics2:part', 16))
-        .fluidInputs(fluid('dye_' + dyeColors[i]) * 36)
+        .fluidInputs(fluid('dye_' + dyeColors[i]) * 18)
         .outputs(item('appliedenergistics2:part', i))
         .EUt(VA[LV])
         .duration(TICK)
@@ -2186,7 +2186,7 @@ for (int i = 20; i < 36; i++) {
     // Covered Cable + Chem Dye -> Covered Cable
     chemical_bath.recipeBuilder()
         .inputs(item('appliedenergistics2:part', 36))
-        .fluidInputs(fluid('dye_' + dyeColors[i - 20]) * 36)
+        .fluidInputs(fluid('dye_' + dyeColors[i - 20]) * 18)
         .outputs(item('appliedenergistics2:part', i))
         .EUt(VA[LV])
         .duration(TICK)
@@ -2207,7 +2207,7 @@ for (int i = 40; i < 56; i++) {
     // Smart Cable + Chem Dye -> Colored Smart Cable
     chemical_bath.recipeBuilder()
         .inputs(item('appliedenergistics2:part', 56))
-        .fluidInputs(fluid('dye_' + dyeColors[i - 40]) * 36)
+        .fluidInputs(fluid('dye_' + dyeColors[i - 40]) * 18)
         .outputs(item('appliedenergistics2:part', i))
         .EUt(VA[LV])
         .duration(TICK)
@@ -2227,7 +2227,7 @@ for (int i = 60; i < 76; i++) {
     // Dense Cable + Chem Dye -> Colored Dense Cable
     chemical_bath.recipeBuilder()
         .inputs(item('appliedenergistics2:part', 76))
-        .fluidInputs(fluid('dye_' + dyeColors[i - 60]) * 36)
+        .fluidInputs(fluid('dye_' + dyeColors[i - 60]) * 18)
         .outputs(item('appliedenergistics2:part', i))
         .EUt(VA[LV])
         .duration(TICK)
